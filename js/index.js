3fd703bff575bf1788c4d66dabc5133ab8399fe1
Dung LT <thync@outlook.com>
2019-01-04 09:39:40 +0700

    Fix

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){
-  $('.modal-trigger').leanModal();
-});
+// Things. And some stuff.
+
+/* Fonts used:
+
+    - Header: "Source Sans Pro"
+    - Icons: "zocial"
+    
+*/
